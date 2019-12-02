f3173d919f7402cf6664eca38dc5bf7101338eeb
Nabil Elidrissi <nabil@paybase.io>
2019-12-02 23:19:57 +0000

    working on update method WIP

--- src/client/components/TableRow.js
+++ src/client/components/TableRow.js
@@ -8,9 +8,7 @@ const TableRow = (props) => {
   // this function is saving the updated data
   const handleEditing = (e, index) => {
     let newArr = [...updatedItems]; // copying the old datas array
-    newArr[index][e.target.name]= e.target.value; 
-
-    // Trigger re-render
+    newArr[index][e.target.name] = e.target.value; 
     setItems(newArr);
   }
 
@@ -22,24 +20,41 @@ const TableRow = (props) => {
   const seEditing = (item, index) => {
     const items = props.items.map(i => ({ ...i, editing: item.editing && i === item }))
     items[index].editing = true;
-    console.log('this item should be true >', items[index]);
     setItems(items);
   }
 
-  useEffect(() => {
-    console.log('>>>>', updatedItems);
-  }, [updatedItems]);
+  // useEffect(() => {
+  //   console.log('useEffect', updatedItems);
+  // }, []);
 
   return (
     props.items.length > 0 ? (props.items.map((item, index) => (
       <tr key={item._id} onClick={(e) => {seEditing(item, index)}}>
-        <td>{item.editing ? <input name="foodName" type="text" onChange={(e) => { handleEditing(e, index) }} defaultValue={item.foodName} /> : item.foodName}</td>
-        <td>{item.editing ? <input name="foodType" type="text" onChange={(e) => { handleEditing(e, index) }} defaultValue={item.foodType} /> : item.foodType}</td>
-        <td>{item.editing ? <input name="purchaseDate" type="text" onChange={(e) => { handleEditing(e, index) }} defaultValue={item.purchaseDate} /> : item.purchaseDate}</td>
-        <td>{item.editing ? <input name="expiryDate" type="text" onChange={(e) => { handleEditing(e, index) }} defaultValue={item.expiryDate} /> : item.expiryDate}</td>
-        <td>{item.editing ? <input name="notes" type="text" onChange={(e) => { handleEditing(e, index) }} defaultValue={item.notes} /> : item.notes}</td>
-        <td><button onClick={() => props.deleteUser(item._id)} type="button" className="btn btn-danger">Delete</button></td>
-        <td><button type="button" onClick={(e) => { !item.editing ? item.editing = true : '' || seEditing(item, index) }} className="btn btn-info">Edit</button></td>
+        <td>{item.editing ? <input name="foodName" type="text" onChange={(e) => 
+          { handleEditing(e, index) }} defaultValue={item.foodName} /> : item.foodName}
+        </td>
+        <td>{item.editing ? <input name="foodType" type="text" onChange={(e) => 
+            { handleEditing(e, index) }} defaultValue={item.foodType} /> : item.foodType}
+        </td>
+        <td>{item.editing ? <input name="purchaseDate" type="text" onChange={(e) => 
+          { handleEditing(e, index) }} defaultValue={item.purchaseDate} /> : item.purchaseDate}
+        </td>
+        <td>{item.editing ? <input name="expiryDate" type="text" onChange={(e) => 
+          { handleEditing(e, index) }} defaultValue={item.expiryDate} /> : item.expiryDate}
+        </td>
+        <td>{item.editing ? <input name="notes" type="text" onChange={(e) => 
+          { handleEditing(e, index) }} defaultValue={item.notes} /> : item.notes}
+        </td>
+        <td>
+          <button onClick={() => 
+            props.deleteUser(item._id)} type="button" className="btn btn-danger">Delete
+          </button>
+        </td>
+        <td>
+          <button type="button" onClick={(e) => 
+          { !item.editing ? item.editing = true : '' || seEditing(item, index) }} className="btn btn-info">Edit
+          </button>
+        </td>
         <td><button type="button" onClick={saveItems} className="btn btn-success">Save</button></td>
       </tr>
     ))) : (<tr><td key={0}>{'No Items'}</td></tr>)
